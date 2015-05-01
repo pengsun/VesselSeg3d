df39aad15b26187863917c4f93e11cb1823ab781
peng sun <pengsun000@gmail.com>
2015-05-01 18:22:22 -0400

    add ub.m

--- script_p3d/ub.m
+++ script_p3d/ub.m
@@ -1,17 +1,17 @@
-function win_tmp ()
+function ub ()
 %% init dag: from scratch
 beg_epoch = 1; 
-dir_data  = 'D:\data\defactoSeg2';
+dir_data  = '/home/ubuntu/A/data/defactoSeg2';
 dir_root  = fileparts( fileparts( mfilename('fullpath') ) );
-dir_mo    = fullfile(dir_root,'mo_zoo','tmp_p3d');
+dir_mo    = fullfile(dir_root,'mo_zoo','p3d');
 
 h = create_dag_from_scratch ();
 h = set_dataNormLayer (h, dir_root);
 %% config
 h.beg_epoch = beg_epoch;
-h.num_epoch = 80;
+h.num_epoch = 1000;
 batch_sz    = 256;
-ni_perMha   = 8e3;
+ni_perMha   = 2e4;
 
 %% CPU or GPU
 % h.the_dag = to_cpu( h.the_dag );
@@ -80,7 +80,7 @@ h.the_dag.tfs{1}.v_std  = st.v_std;
 
 function tr_bdg = load_tr_data(dir_data, ni_perMha, bs)
 % load the info file and make the names list
-st = load( fullfile(dir_data, 'info_med.mat') );
+st = load( fullfile(dir_data, 'info.mat') );
 names = st.imgNames(st.imgSetId==1); % 1 indicates training data
 names = cellfun( @(nm)(fullfile(dir_data, nm)), ...
   names, 'UniformOutput', false); 
